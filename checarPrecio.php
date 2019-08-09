36d1fc9cbd6bcf0c5a2f3f2a488a952267f8e07b
Robbie Reta <robbie.reta67@gmail.com>
2019-08-09 16:46:02 -0500

    ya checa precio!

--- checarPrecio.php
+++ checarPrecio.php
@@ -9,37 +9,29 @@ if($link === false){
 }
 if(isset($_POST["codigo"])){
     $codigo=$_POST["codigo"];
-    echo $codigo;
+    echo  "codigo del producto :".$codigo;
 }
-// // Attempt select query execution
-// $sql = "SELECT * FROM persons";
-// if($result = mysqli_query($link, $sql)){
-//     if(mysqli_num_rows($result) > 0){
-//         echo "<table>";
-//             echo "<tr>";
-//                 echo "<th>id</th>";
-//                 echo "<th>first_name</th>";
-//                 echo "<th>last_name</th>";
-//                 echo "<th>email</th>";
-//             echo "</tr>";
-//         while($row = mysqli_fetch_array($result)){
-//             echo "<tr>";
-//                 echo "<td>" . $row['id'] . "</td>";
-//                 echo "<td>" . $row['first_name'] . "</td>";
-//                 echo "<td>" . $row['last_name'] . "</td>";
-//                 echo "<td>" . $row['email'] . "</td>";
-//             echo "</tr>";
-//         }
-//         echo "</table>";
-//         // Free result set
-//         mysqli_free_result($result);
-//     } else{
-//         echo "No records matching your query were found.";
-//     }
-// } else{
-//     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-// }
+// Attempt select query execution
+$sql = "SELECT * FROM productos WHERE codigo='$codigo'";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<hr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td> Precio del producto: " . $row['precio'] . "</td>";
+               
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
  
-// Close connection
+//Close connection
 mysqli_close($link);
 ?>
