35947761d251873a83d13411b35fa21aaf4a5394
Robbie Reta <robbie.reta67@gmail.com>
2019-08-13 12:48:03 -0500

    por fin jala el select ,ya carga la lista

--- index.php
+++ index.php
@@ -23,30 +23,28 @@
     Selecciona una moto para consultar sus piezas:
     <br>
     <br>
-    <form id="myForm" action="comment.php" method="post">
-    <label for="atom">Atom 150</label>
-    <select name="" id="atom">
-    <option value="atom">Seleciona una parte</option>
-    <optgroup label="Motor">
-        <option>Cabeza y clindro</option>
-    </optgroup>
-    </select>
+    <form id="form1" method="post">
+    <label >Atom 150</label>
+    <select id="location" onchange="setIframeSource()"> 
+    <option selected value="relleno.html">Por favor, selecciona una parte</option>
+    <option value="https://docs.google.com/spreadsheets/d/e/2PACX-1vQrGviYJkQ4UHnZmZiz2yjQPEEkhAV9mK7iPXtGKZOfr4TshQ5nMJNL7UzqMySudyY30Iz75rJSkspb/pubhtml?gid=778975291&amp;single=true&amp;widget=true&amp;headers=false">Cilindro</option>
+  </select>
    
-  <input type="submit" value="Mostrar piezas">
   </form>
     <div></div>
     
     <!--     -->
     <br>
-    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQrGviYJkQ4UHnZmZiz2yjQPEEkhAV9mK7iPXtGKZOfr4TshQ5nMJNL7UzqMySudyY30Iz75rJSkspb/pubhtml?gid=778975291&amp;single=true&amp;widget=true&amp;headers=false"></iframe>    
+    <iframe id="myIframe" src="relleno.html"></iframe>    
 </body>
 <script> 
-        // wait for the DOM to be loaded 
-        $(document).ready(function() { 
-            // bind 'myForm' and provide a simple callback function 
-            $('#myForm').ajaxForm(function() { 
-                alert("Thank you for your comment!"); 
-            }); 
-        }); 
+   function setIframeSource() {
+   var theSelect = document.getElementById('location');
+   var theIframe = document.getElementById('myIframe');
+   var theUrl;
+ 
+   theUrl = theSelect.options[theSelect.selectedIndex].value;
+   theIframe.src = theUrl;
+}
     </script> 
 </html>
