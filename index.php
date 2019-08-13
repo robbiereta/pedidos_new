2aaabfd5449d365e0964f387a8b04eaee029f246
Robbie Reta <robbie.reta67@gmail.com>
2019-08-13 18:47:56 -0500

    listo para init y deploy en firebase

--- index.php
+++ index.php
@@ -37,6 +37,14 @@
     <br>
     <iframe id="myIframe" src="relleno.html"></iframe>    
 </body>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/6.3.5/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#reserved-urls -->
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>
 <script> 
    function setIframeSource() {
    var theSelect = document.getElementById('location');
