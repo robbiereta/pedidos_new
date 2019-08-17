c50048c38eb7e91e7448a9b02d44db6d1f5b1bd6
Robbie Reta <robbie.reta67@gmail.com>
2019-08-17 13:45:48 -0500

    varios manuales listos , falta ponerle el link de los manuales y las lista de precios

--- index.php
+++ index.php
@@ -63,11 +63,186 @@
     </optgroup>
   </select>
 <br>
-<label for="">Classic 125</label>
+<!-- <label for="">Classic 125</label>
   <select name="classic" id="">
   <option value="">Seleccciona</option>
   </select>
-   
+    -->
+  
+    <span>Classic 125</span>
+    <select id="location2" onchange="setIframeSource2()">	
+    <option selected value="relleno.html">Por favor, selecciona una parte</option>	
+<optgroup label="MOTOR">	MOTOR	</optgroup>
+<option>	CABEZA Y CILINDRO	</option>
+<option>	VÁLVULAS Y BALANCINES	</option>
+<option>	CARTERS DE MOTOR	</option>
+<option>	CLUTCH	</option>
+<option>	BOMBA DE ACEITE	</option>
+<option>	CUBIERTAS DE MOTOR	</option>
+<option>	CIGÜEÑAL,PISTÓN Y BIELA	</option>
+<option>	TRANSMISION	</option>
+<option>	SELECTOR DE CAMBIOS	</option>
+<option>	MAGNETO	</option>
+<option>	MOTOR DE ARRANQUE	</option>
+<option>	CARBURADOR	</option>
+<optgroup label="PLASTICOS">	PLÁSTICOS	</optgroup>
+<option>	PLASTICOS	</option>
+<optgroup label="ELECTRICO">	ELÉCTRICO	</optgroup>
+<option>	FARO, TABLERO,Y STOP	</option>
+<option>	SISTEMA ELECTRICO Y COMPONENTES	</option>
+<option>	CONTROL DE MANDOS Y CHICOTES	</option>
+<optgroup label="CHASIS">	CHASIS	</optgroup>
+<option>	POSAPIE,PEDALES Y PARADORES	</option>
+<option>	FILTRO DE AIRE Y ESCAPE	</option>
+<option>	TANQUE DE COMBUSTIBLE	</option>
+<option>	BARRAS Y AMORTIGUADOR	</option>
+<option>	YUGO Y MANUBRIO	</option>
+<option>	LLANTA Y FRENO DELANTERO	</option>
+<option>	HORQUILLA	</option>
+<option>	LLANTA Y FRENO TRASERO	</option>
+<option>	CUADRO	</option>
+</select>		
+<br>
+<span>Partner 150</span>
+    <select id="location3" onchange="setIframeSource3()">
+    <option selected value="relleno.html">Por favor, selecciona una parte</option>	
+    <optgroup label="MOTOR">	MOTOR			</optgroup>
+<option>	CABEZA Y CILINDRO			</option>
+<option>	VÁLVULAS Y BALANCINES			</option>
+<option>	CARTERS DE MOTOR			</option>
+<option>	CLUTCH			</option>
+<option>	BOMBA DE ACEITE			</option>
+<option>	CUBIERTAS DE MOTOR			</option>
+<option>	CIGÜEÑAL Y PISTON			</option>
+<option>	TRANSMISION			</option>
+<option>	SELECTOR DE CAMBIOS			</option>
+<option>	MAGNETO			</option>
+<option>	MOTOR DE ARRANQUE			</option>
+<option>	CARBURADOR			</option>
+<optgroup label="ELECTRICO">	SISTEMA ELECTRICO			</optgroup>
+<option>	TABLERO			</option>
+<option>	SISTEMA ELECTRICO Y COMPONENTES			</option>
+<option>	STOP Y FARO Y DIRECCIONALES			</option>
+<optgroup label="PLASTICOS">	PLASTICOS			</optgroup>
+<option>	PLASTICOS			</option>
+<optgroup label="CHASIS">	CHASIS			</optgroup>
+<option>	MANDOS Y CHICOTES			</option>
+<option>	MANUBRIO			</option>
+<option>	YUGO Y BARRAS DELANTERAS			</option>
+<option>	TANQUE DE COMBUSTIBLE			</option>
+<option>	ESCAPE			</option>
+<option>	HORQUILLA Y AMORTIGUADORES			</option>
+<option>	RUEDA DELANTERA Y TRASERA			</option>
+<option>	PEDALES Y PARADORES			</option>
+<option>	FILTRO DE AIRE			</option>
+<option>	CUADRO			</option>
+    </select>
+
+
+    <br>
+    <span>Spartha 2</span>
+    <select id="location4" onchange="setIframeSource4()">
+    <option selected value="relleno.html">Por favor, selecciona una parte</option>	
+    <optgroup label="MOTOR">	MOTOR	</optgroup>
+<option>	CABEZA Y CILINDRO	</option>
+<option>	VALVULAS Y BALANCINES	</option>
+<option>	CARTERS DE MOTOR	</option>
+<option>	CLUTCH	</option>
+<option>	BOMBA DE ACEITE Y TENSOR DE CADENA	</option>
+<option>	CUBIERTAS DE CARTERS	</option>
+<option>	CIGÜEÑAL Y PISTON	</option>
+<option>	TRANSMISION Y FLECHA DE ARRANQUE	</option>
+<option>	SELECTOR DE CAMBIOS	</option>
+<option>	MOTOR DE ARRANQUE	</option>
+<option>	MAGNETO	</option>
+<option>	CARBURADOR	</option>
+<optgroup label="PLASTICOS">	PLÁSTICOS	</optgroup>
+<option>	PLASTICOS	</option>
+<optgroup label="ELECTRICO">	ELÉCTRICO	</optgroup>
+<option>	FARO TABLERO DIRECCIONALES Y STOP	</option>
+<option>	SISTEMA ELECTRICO Y COMPONENTES	</option>
+<option>	CONTROL DE MANDOS	</option>
+<optgroup label="CHASIS">	CHASIS	</optgroup>
+<option>	POSAPIES Y PEDALES	</option>
+<option>	FILTRO Y ESCAPE	</option>
+<option>	TANQUE DE COMBUSTIBLE	</option>
+<option>	BARRAS DELANTERAS Y AMORTIGUADOR	</option>
+<option>	YUGO	</option>
+<option>	LLANTA Y FRENO DELANTERO	</option>
+<option>	HORQUILLA	</option>
+<option>	LLANTA Y FRENO TRASERO	</option>
+<option>	CUADRO	</option>
+    </select>
+
+    <br>
+    <span>Fighter 200</span>
+    <select id="location5" onchange="setIframeSource5()">
+    <option selected value="relleno.html">Por favor, selecciona una parte</option>
+    <optgroup label="MOTOR">	MOTOR			</optgroup>
+<option>	CABEZA Y CILINDRO			</option>
+<option>	VALVULAS Y BALANCINES			</option>
+<option>	CARTERS DE MOTOR			</option>
+<option>	CLUTCH			</option>
+<option>	BOMBA DE ACEITE			</option>
+<option>	CUBIERTAS DE CARTER			</option>
+<option>	CIGÜEÑAL			</option>
+<option>	TRANSMISON			</option>
+<option>	SELECTOR DE CAMBIOS			</option>
+<option>	MOTOR DE ARRANQUE			</option>
+<option>	MAGNETO			</option>
+<option>	CARBURADOR			</option>
+<optgroup label="PLASTICOS">	PLÁSTICOS			</optgroup>
+<option>	PLASTICOS			</option>
+<optgroup label="ELECTRICO">	ELÉCTRICO			</optgroup>
+<option>	FARO, TABLERO, DIRECCIONALES Y STOP			</option>
+<option>	SISTEMA ELECTRICO Y COMPONENTES			</option>
+<option>	CONTROL DE MANDOS Y CHICOTES			</option>
+<optgroup label="CHASIS">	CHASIS			</optgroup>
+<option>	POSAPIES Y PEDALES			</option>
+<option>	FILTRO DE AIRE Y ESCAPE			</option>
+<option>	TANQUE DE COMBUSTIBLE			</option>
+<option>	BARRAS Y AMORTIGUADORE			</option>
+<option>	YUGO Y MANUBRIO			</option>
+<option>	LLANTA Y FRENO DELANTERO			</option>
+<option>	HORQUILLA			</option>
+<option>	LLANTA Y FRENO TRASERO			</option>
+<option>	CUADRO			</option>	
+    </select>
+
+    <br>
+    <span>Bulldog 175</span>
+    <select id="location6" onchange="setIframeSource6()">
+    <option selected value="relleno.html">Por favor, selecciona una parte</option>
+    <optgroup label="MOTOR">	MOTOR			</optgroup>
+<option>	CABEZA Y CILINDRO			</option>
+<option>	VALVULAS Y BALANCINES			</option>
+<option>	CARTERS DE MOTOR			</option>
+<option>	CLUTCH			</option>
+<option>	BOMBA DE ACEITE			</option>
+<option>	CUBIERTAS DE CARTERS			</option>
+<option>	CIGÜEÑAL Y PISTON			</option>
+<option>	TRANSMISION			</option>
+<option>	SELECTOR DE CAMBIOS			</option>
+<option>	MAGNETO			</option>
+<option>	MOTOR DE ARRANQUE			</option>
+<option>	CARBURADOR			</option>
+<optgroup label="PLASTICOS">	PLÁSTICOS			</optgroup>
+<option>	PLASTICOS			</option>
+<optgroup label="ELECTRICO">	ELÉCTRICO			</optgroup>
+<option>	FARO,TABLERO,DIRECCIONALES Y STOP			</option>
+<option>	SISTEMA ELECTRICO Y COMPONENTES			</option>
+<option>	CONTROL DE MANDOS,MANUBRIO Y CHICOTES			</option>
+<optgroup label="CHASIS">	CHASIS			</optgroup>
+<option>	POSAPIES,PEDALES Y PARADORES			</option>
+<option>	FILTRO DE AIRE Y ESCAPE			</option>
+<option>	TANQUE DE COMBUSTIBLE			</option>
+<option>	BARRAS Y AMORTIGUADOR			</option>
+<option>	YUGO			</option>
+<option>	LLANTA Y FRENO DELANTERO			</option>
+<option>	HORQUILLA			</option>
+<option>	LLANTA Y FRENO TRASERO			</option>
+<option>	CUADRO			</option>
+</select>
   </form>
     <div></div>
 
@@ -93,6 +268,46 @@
 <script> 
    function setIframeSource() {
    var theSelect = document.getElementById('location');
+   var theIframe = document.getElementById('myIframe');
+   var theUrl;
+ 
+   theUrl = theSelect.options[theSelect.selectedIndex].value;
+   theIframe.src = theUrl;
+}
+function setIframeSource2() {
+   var theSelect = document.getElementById('location2');
+   var theIframe = document.getElementById('myIframe');
+   var theUrl;
+ 
+   theUrl = theSelect.options[theSelect.selectedIndex].value;
+   theIframe.src = theUrl;
+}
+function setIframeSource3() {
+   var theSelect = document.getElementById('location3');
+   var theIframe = document.getElementById('myIframe');
+   var theUrl;
+ 
+   theUrl = theSelect.options[theSelect.selectedIndex].value;
+   theIframe.src = theUrl;
+}
+function setIframeSource4() {
+   var theSelect = document.getElementById('location4');
+   var theIframe = document.getElementById('myIframe');
+   var theUrl;
+ 
+   theUrl = theSelect.options[theSelect.selectedIndex].value;
+   theIframe.src = theUrl;
+}
+function setIframeSource5() {
+   var theSelect = document.getElementById('location5');
+   var theIframe = document.getElementById('myIframe');
+   var theUrl;
+ 
+   theUrl = theSelect.options[theSelect.selectedIndex].value;
+   theIframe.src = theUrl;
+}
+function setIframeSource6() {
+   var theSelect = document.getElementById('location6');
    var theIframe = document.getElementById('myIframe');
    var theUrl;
  
